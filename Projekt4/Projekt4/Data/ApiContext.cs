c99b82023d67145ebd4b03ab3f635900f8311a79
jakub-buczko <117637949+jakub-buczko@users.noreply.github.com>
2023-03-28 14:18:37 +0200

    Add files via upload

---
+++ Projekt4/Projekt4/Data/ApiContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Projekt4.Controllers;
+using Projekt4.Models;
+
+
+namespace Projekt4.Data
+{
+    public class ApiContext : DbContext
+    {
+        public DbSet<HotelBooking> Bookings { get; set; }
+
+        public ApiContext(DbContextOptions<ApiContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ Projekt4/Projekt4/Models/HotelBooking.cs
@@ -0,0 +1,10 @@
+﻿namespace Projekt4.Models
+{
+    public class HotelBooking
+    {
+        public int Id { get; set; }
+        public int RoomNumber { get; set; }
+        public string? ClientName { get; set; }
+
+    }
+}
